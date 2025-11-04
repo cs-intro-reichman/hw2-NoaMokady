1115e13e56161d9259c519944680bf4d208063b2
Noa Mokady <noa.mokady@post.runi.ac.il>
2025-11-04 18:37:08 +0200

    Finished exercise 2

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,14 +1,17 @@
 public class TimeCalc {
     public static void main(String[] args) {
+        // Gets the time stamp and the added minutes from the user.
         int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
 
+        // Calculates the new hours and minutes after adding the given minutes.
         int minutesToAdd = Integer.parseInt(args[1]);
         int totalMinutes = (hours * 60) + minutes + minutesToAdd;
         int totalHours = totalMinutes / 60;
         int newHours = totalHours % 24;
         int newMinutes = totalMinutes - (totalHours * 60);
         
+        // Prints the new time stamp.
         String message = newHours + ":" + newMinutes;
         System.err.println(message);
     }
